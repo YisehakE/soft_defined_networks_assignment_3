ef1714e203793d03d627acee307ac2bb321e6548
YisehakE <yisehakebrahim02@gmail.com>
2024-03-29 12:32:14 -0400

    Implementing setup QoS along with ingesting firewall policies from input

--- Submission_files/controller.py
+++ Submission_files/controller.py
@@ -17,6 +17,8 @@ from pox.lib.revent import *
 from pox.lib.util import dpid_to_str
 from pox.lib.addresses import IPAddr, EthAddr
 
+import pox.lib.packet as pkt
+
 log = core.getLogger()
 
 
@@ -26,6 +28,11 @@ TODO(s) to account for
   TODO #1 - Figure out how to get firewalls to controller (maybe as argument?)
   TODO #2 - Find the function to parse packet using POX 
   TODO #3 - 
+
+Questions (MOVED TO GOOGLE DOC)
+  #1 - 
+  #2 - 
+  #3 - 
 
 
 '''
@@ -46,8 +53,10 @@ class Controller(EventMixin):
 
         self.FIREWALL_POLICIES = [] # MY TODO: Figure out how to actualize this into the class for event handler for connection
         
+
+    
     # You can write other functions as you need.
-        
+    ######################################################################################  
     def getPolicyContent(self, contents):
       n_rules = int(contents[0])
       m_hosts = int(contents[1])
@@ -55,24 +64,45 @@ class Controller(EventMixin):
       hosts = contents[2 + n_rules:]
 
       return n_rules, m_hosts, rules, hosts
+    
 
-        
+    def is_packet_IP(self, packet):
+      ip = packet.find('ipv4')
+
+      if ip is None:  # This packet isn't IP!
+        return False
+      else:
+        print("Source IP: ", ip.srcip)
+        return True
+
+
+      
+    ######################################################################################
         
     def _handle_PacketIn (self, event):    
         
     	# install entries to the route table
         def install_enqueue(event, packet, outport, q_id): 
+
+          # TODO 3 - Attach a reference mark to the documentation where commands were found
+
+          # Initialize/create message using flow modification table 
+          # (Ref. OpenFlow in POX -> OpenFlow Messages -> ofp_flow_mod ) https://noxrepo.github.io/pox-doc/html/#ofp-flow-mod-flow-table-modification)
           msg = of.ofp_flow_mod()
 
-          # parsed_pkt = event.parsed() # TODO 2 - Find the function to parse packet using POX 
-
-          enqueue_action = of.ofo_action_enqueue(port = outport, 
-                                queue_id=q_id)
-          
-          
+          # Set the match structure to given packet with the input port obtained from the raised event
+          # (Ref. OpenFlow in POX -> Match Structure -> ofp_match methods) https://noxrepo.github.io/pox-doc/html/#ofp-match-methods)
           msg.match = of.ofp_match.from_packet(packet, event.port)
+
+
+          # Create action for enqueuing actions in flow table
+          # (Ref. OpenFlow in POX -> Match Structure -> ofp_match methods) https://noxrepo.github.io/pox-doc/html/#ofp-match-methods)
+          enqueue_action = of.ofo_action_enqueue(port = outport, 
+                                                 queue_id=q_id)
           msg.actions.append(enqueue_action)
           
+
+          # Send this message from this switch to the controller 
           event.connection.send(msg)
           
 
@@ -81,16 +111,45 @@ class Controller(EventMixin):
           print("Message content[FORWARD]: ", message)
           log.debug("Message content [FORWARD]", message)
 
-          packet = event.parsed 
+          pkt = event.parsed # TODO 2 - Find the function to parse packet using POX 
+
+          # Check what protocol the packet is
+          if self.is_packet_IP(pkt):
+            # TODO: figure out how 
+
+            # Check to see if packet falls under any of the firewall policies 
+            src_addr = pkt.srcip
+            dst_addr = pkt.dstip
 
 
-          if packet.TYPE == 
-          if message:
-            print("Message content: ", message)
-
-            #TODO: extract values from message
+            if (dst_addr, PORT) in self.FIREWALL_POLICIES: # 1st type of firewall 
+               pass
+            elif (src_addr, dst_addr, PORT) in self.FIREWALL_POLICIES: # 2nd type of firewall
+               pass
+            else:
+            
+              # TODO: see what & how to send this message OR packet if it's safe from firewall restrictions
+              # Send this message from this switch to the controller 
+              event.connection.send(message)
+            
           else:
-            print("No message!")
+                # TODO: figure out if 1) this would ever be the case and 2) what to do if pkt is NOT IP
+             pass
+              
+          
+          
+          # # TODO: see what format the message would come in 
+          # # TODO: see if checking message is necessary
+          # if message:
+          #   print("Message content: ", message)
+
+          #   #TODO: extract values from message
+          # else:
+          #   print("No message!")
+
+
+          event.connection.send(msg)
+
             
 
 
@@ -99,9 +158,20 @@ class Controller(EventMixin):
           print("Message content [FLOOD]: ", message)
           log.debug("Message content [FORWARD]", message)
 
-
           # define your message here
           msg = of.ofp_flow_mod()
+
+          if not message.match.dl or (not message.match.nw_dst and not message.match.tp_dst):
+            # Case 1: Nothing is known about the destination from the message match structure
+            # Essentially, somewhat like ff:ff:ff:ff:ff or 255.255.255.0 as the broadcast address in ARP or DHCP
+            msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+  
+          else:
+             # Case 2: Message destination is known from match structure 
+            msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+            pkt = event.parsed
+            install_enqueue(event, pkt, event.)
+        
 
           # ofp_action_output: forwarding packets out of a physical or virtual port
           # OFPP_FLOOD: output all openflow ports expect the input port and those with flooding disabled via the OFPPC_NO_FLOOD port config bit
